aedd403c90ac889dafbbed2f38c049910e335c38
Rick Bollar <GitHub.com@bol.la>
2025-12-26 16:17:09 +1000

    Initial Commit

---
+++ PowstonTests/PowstonTests.swift
@@ -0,0 +1,16 @@
+//
+//  PowstonTests.swift
+//  PowstonTests
+//
+//  Created by Rick Bollar on 12/26/25.
+//
+
+import Testing
+
+struct PowstonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
